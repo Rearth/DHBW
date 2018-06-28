99285b134f46906a3e4c016764bafea2ce7ca0b5
david <davidus2010@gmail.com>
2018-06-28 10:20:42 +0200

    first javafx stuff

---
+++ src/sem02/de/dhbw/fx/fxeample.java
@@ -0,0 +1,16 @@
+package sem02.de.dhbw.fx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class fxeample extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
